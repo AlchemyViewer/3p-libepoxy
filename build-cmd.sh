a438efc8fb986c309de5a94cb243d9561b0bd338
Rye Mutt <rye@bred.dog>
2020-08-16 17:51:57 -0400

    Add autobuild skeleton

---
+++ build-cmd.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+exec 4>&1; export BASH_XTRACEFD=4; set -x
+# make errors fatal
+set -e
+# complain about unset env variables
+set -u
+
+if [ -z "$AUTOBUILD" ] ; then 
+    exit 1
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    autobuild="$(cygpath -u $AUTOBUILD)"
+else
+    autobuild="$AUTOBUILD"
+fi
+
+top="$(pwd)"
+stage="$(pwd)/stage"
+
+mkdir -p $stage
+
+# Load autobuild provided shell functions and variables
+source_environment_tempfile="$stage/source_environment.sh"
+"$autobuild" source_environment > "$source_environment_tempfile"
+. "$source_environment_tempfile"
+
+EPOXY_VERSION="1.5.4"
+EPOXY_SOURCE_DIR="libepoxy"
+
+VERSION_HEADER_FILE="$stage/include/epoxy/config.h"
+
+build=${AUTOBUILD_BUILD_ID:=0}
+
+# Create the staging folders
+mkdir -p "$stage/lib"/{debug,release,relwithdebinfo}
+mkdir -p "$stage/include/epoxy"
+mkdir -p "$stage/LICENSES"
+
+pushd "$EPOXY_SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+
+        # ------------------------ windows, windows64 ------------------------
+        windows*)
+            load_vsvars
+
+            if [ "$AUTOBUILD_ADDRSIZE" = 32 ]
+            then
+                archflags=""
+            else
+                archflags="/arch:AVX"
+            fi
+
+            meson build "_build" --prefix="${stage}"
+
+            mkdir -p "_build"
+            pushd "_build"
+                ninja
+                ninja install
+            popd
+        ;;
+
+        "darwin")
+        ;;
+        linux*)
+        ;;
+    esac
+    mkdir -p "$stage/LICENSES"
+    cp COPYING "$stage/LICENSES/libepoxy.txt"
+popd
+
+# version will be (e.g.) "1.4.0"
+version=`sed -n -E 's/#define PACKAGE_VERSION "([0-9])[.]([0-9])[.]([0-9]).*/\1.\2.\3/p' "${VERSION_HEADER_FILE}"`
+# shortver will be (e.g.) "230": eliminate all '.' chars
+#since the libs do not use micro in their filenames, chop off shortver at minor
+short="$(echo $version | cut -d"." -f1-2)"
+shortver="${short//.}"
+
+echo "${version}.${build}" > "${stage}/VERSION.txt"
